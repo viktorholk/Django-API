f4ad6e79a8b7968eeef228f0a3910002cb67de19
viktorholk <viktorholk@gmail.com>
2020-11-11 14:19:37 +0100

    .

--- main/urls.py
+++ main/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    path('', include('main.urls'))
+    path('', views.basepoint)
 ]
