cf35e1ff40dd9cd319b27e174f6aab601a0d24d7
viktorholk <viktorholk@gmail.com>
2020-11-12 10:06:52 +0100

    .

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=65)
+    author = models.CharField(max_length=65)
+    release_data = models.DateField()
+    pages = models.IntegerField()
+
+    def __str__(self):
+        return self.title
