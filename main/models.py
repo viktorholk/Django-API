743672a55c567a066d73bf1e04f10f33a75baa59
viktorholk <viktorholk@gmail.com>
2020-11-12 10:28:15 +0100

    release_date

--- main/models.py
+++ main/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=65)
     author = models.CharField(max_length=65)
-    release_data = models.DateField()
+    release_date = models.DateTimeField()
     pages = models.IntegerField()
 
     def __str__(self):
