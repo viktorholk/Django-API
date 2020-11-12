654921516a29000a326232628a88f8b33ac12179
viktorholk <viktorholk@gmail.com>
2020-11-12 10:29:08 +0100

    BookViewSet

--- main/views.py
+++ main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework import viewsets
+from .serializers import BookSerializer
+from .models import Book
 # Create your views here.
 def basepoint(request):
-    return JsonResponse({"hello": "world"});
+    return JsonResponse({"hello": "world"})
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
