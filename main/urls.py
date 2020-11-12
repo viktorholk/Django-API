d05ddeac83ef9c7b997caa9658de766a3586b289
viktorholk <viktorholk@gmail.com>
2020-11-12 10:29:01 +0100

    router

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.urls import path, include
 from . import views
 
+from rest_framework import routers
+router = routers.DefaultRouter()
+router.register(r'books', views.BookViewSet)
+
 urlpatterns = [
-    path('', views.basepoint)
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
