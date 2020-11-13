2b5f0cdb69b7ee00c63f8fb50f61c5deef3acb38
viktorholk <viktorholk@gmail.com>
2020-11-13 11:27:32 +0100

    removed default django apps

--- api/settings.py
+++ api/settings.py
@@ -32,13 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main',
-    
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
 ]
