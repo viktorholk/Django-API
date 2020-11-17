94bb003a245aa99c2014e0fc959d4194ef736fb0
viktorholk <viktorholk@gmail.com>
2020-11-17 08:38:46 +0100

    Node user and manager

--- api/models.py
+++ api/models.py
@@ -3,28 +3,51 @@ from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser )
 # Create your models here.
 
 
-    
-class NodeManager(BaseUserManager):
-    def create_node(self, title, password=None):
-        if not title:
-            raise ValueError("Nodes must have a title")
-    
-        node = self.model(
-            title=title
+class NodeUserManager(BaseUserManager):
+    def create_user(self, node, password=None):
+        if not node:
+                raise ValueError('Users must have an node name')
+        
+        user = self.model(
+            node=node
         )
-        node.set_password(password)
-        node.save(using=self._db)
-        return node
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, node, password=None):
+        user = self.create_user(
+            node
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
-class Node(AbstractBaseUser):
-    title       = models.CharField(max_length=45, unique=True)
-    created_at  = models.DateTimeField(auto_now_add=True)
+class NodeUser(AbstractBaseUser):
+    node = models.CharField(max_length=45, unique=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    USERNAME_FIELD  = 'title'
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = NodeUserManager()
+
+    USERNAME_FIELD  = 'node'
     REQUIRED_FIELDS = []
 
-    objects = NodeManager()
-
     def __str__(self):
-        return self.title
+        return self.node
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
