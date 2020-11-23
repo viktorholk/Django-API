f0fd8a3190c704496b7eb8e3a74fb2b9ba49cd7e
viktorholk <viktorholk@gmail.com>
2020-11-23 08:44:02 +0100

    uncommented custom models since they wont authenticate

--- api/models.py
+++ api/models.py
@@ -2,45 +2,46 @@ from django.db import models
 from django.contrib.auth.models import ( BaseUserManager, AbstractBaseUser )
 # Create your models here.
 
-class UserManager(BaseUserManager):
-    def create_user(self, user, password=None):
-        if not user:
-            raise ValueError('Invalid User')
+# class UserManager(BaseUserManager):
+#     def create_user(self, username, password=None):
+#         if not username:
+#             raise ValueError('Invalid User')
     
-        user = self.model(
-            user
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+#         user = self.model(
+#             username
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
     
-    def create_superuser(self, user, password=None):
-        user = self.create_user(user)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+#     def create_superuser(self, username, password=None):
+#         user = self.create_user(username)
+#         user.is_admin = True
+#         user.save(using=self._db)
+#         return user
 
-class User(AbstractBaseUser):
-    username    = models.CharField(max_length=45, unique=True, null=False)
-    created_at  = models.DateTimeField(auto_now_add=True)
+# class User(AbstractBaseUser):
+#     username    = models.CharField(max_length=45, unique=True, null=False)
+#     created_at  = models.DateTimeField(auto_now_add=True)
 
-    is_active   = models.BooleanField(default=True)
-    is_admin    = models.BooleanField(default=False)
+#     is_active   = models.BooleanField(default=True)
+#     is_admin    = models.BooleanField(default=False)
 
-    objects = UserManager()
 
-    USERNAME_FIELD  = 'username'
-    REQUIRED_FIELDS = []
+#     objects = UserManager()
 
-    def __str__(self):
-        return self.username
+#     USERNAME_FIELD  = 'username'
+#     REQUIRED_FIELDS = []
 
-    def has_perm(self, perm, obj=None):
-        return True
+#     def __str__(self):
+#         return self.username
 
-    def has_module_perms(self, app_label):
-        return True
+#     def has_perm(self, perm, obj=None):
+#         return True
 
-    @property
-    def is_staff(self):
-        return self.is_admin
+#     def has_module_perms(self, app_label):
+#         return True
+
+#     @property
+#     def is_staff(self):
+#         return self.is_admin
--- api/views.py
+++ api/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login, authenticate
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+# from .models import UserManager
 # Create your views here.
 class ExampleView(APIView):
     authentication_classes = [TokenAuthentication]
@@ -17,6 +18,15 @@ class Login(APIView):
         if 'username' in request.POST and 'password' in request.POST:
             username    = request.POST['username']
             password    = request.POST['password']
-            print((username, password))
             return HttpResponse(authenticate(username=username, password=password))
-        return HttpResponse('No user infomation was provided')
+        return HttpResponse('Username and password field must be provided')
+
+class Register(APIView):
+    def post(self, request):
+        if 'username' in request.POST and 'password' in request.POST:
+            username    = request.POST['username']
+            password    = request.POST['password']
+            UserManager.create_user(username, password)
+            return HttpResponse('User created')
+        else:
+            return HttpResponse('Username and password field must be provided')
