4d0799dd16f475546e26bc5327c56a8be5b3647a
viktorholk <viktorholk@gmail.com>
2020-11-23 09:55:34 +0100

    ObtainExpiringAuthToken

--- api/views.py
+++ api/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
-from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.views import APIView, Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 # from .models import UserManager
 # Create your views here.
 class ExampleView(APIView):
@@ -34,3 +36,20 @@ class GetToken(APIView):
                 print(token[0])
                 return HttpResponse()
             return HttpResponse('error')
+
+
+class ObtainExpiringAuthToken(ObtainAuthToken):
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            token, created =  Token.objects.get_or_create(user=serializer.validated_data['user'])
+
+            if not created:
+                # update the created time of the token to keep it valid
+                token.created = datetime.datetime.utcnow()
+                token.save()
+
+            return Response({'token': token.key})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+obtain_expiring_auth_token = ObtainExpiringAuthToken.as_view()
